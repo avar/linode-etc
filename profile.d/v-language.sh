e058cfa1c5154622e26d5f75b31ce8c5cc835ac2
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-07-02 16:11:29 +0000

    profile: LANG=en_US.utf8, not en_US.UTF-8. Also only set it if the locale exists

--- profile.d/v-language.sh
+++ profile.d/v-language.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # English motherfucker, do you speak it?
-if test -z "$LANG" && test -z "$LANUAGE"
+if test -z "$LANG" && test -z "$LANUAGE" && locale -a | grep -q en_US.utf8
 then
     LANGUAGE="en_US:en"
-    LANG="en_US.UTF-8"
+    LANG="en_US.utf8"
     export LANGUAGE LANG
 fi
