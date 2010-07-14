e0b71fcc42157b7af1df12581cad52a4333722b7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-07-14 21:42:22 +0000

    bind/t: Don't run tests for the public directory

--- bind/bin/benchmark-slaves.pl
+++ bind/bin/benchmark-slaves.pl
@@ -7,7 +7,7 @@ my $count = $ARGV[0] // 5;
 my $j     = $ARGV[1] // 10;
 my $testdir = "/etc/bind/t/gen";
 
-my @ns = map { s[.*/][]; $_ } glob "$testdir/*";
+my @ns = grep { $_ =~ /^ns/ } map { s[.*/][]; $_ } glob "$testdir/*";
 
 my %test;
 
