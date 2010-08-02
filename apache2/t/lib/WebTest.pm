c3496cf1ac024161dd6a6d2b46ebb73e1563e3b6
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-08-02 18:32:32 +0000

    apache/t: Convert apache tests to run in parallel

---
+++ apache2/t/lib/WebTest.pm
@@ -0,0 +1,36 @@
+package WebTest;
+use 5.10.0;
+use strict;
+use Test::More;
+use Test::WWW::Mechanize;
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $self = {
+         %args,
+    };
+
+    bless $self => $class;
+}
+
+sub run_tests {
+    my ($self) = @_;
+
+    my $domain       = $self->{domain};
+    my $content_like = $self->{content_like};
+
+    plan( tests => 2 );
+
+    my $mech = Test::WWW::Mechanize->new(
+        agent => __FILE__
+    );
+
+    my $site = "http://$domain/";
+    $mech->get_ok( $site );
+    $mech->content_like( $content_like, "$site has content like $content_like");
+
+    return;
+}
+
+1;
