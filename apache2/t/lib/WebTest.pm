ec0933fb8555fb9b0e4be2597dd322c2ad807999
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-08-02 19:43:22 +0000

    apache2/t: Support success, allows testing of ap.nix.is

--- apache2/t/lib/WebTest.pm
+++ apache2/t/lib/WebTest.pm
@@ -20,16 +20,22 @@ sub run_tests {
     my $domain       = $self->{domain};
     my $content_like = $self->{content_like};
     my $base         = $self->{base_url};
+    my $success      = $self->{success};
     my $site         = "http://$domain/";
     my $base_is      = $base // $site;
 
     plan( tests => 3 );
 
     my $mech = Test::WWW::Mechanize->new(
-        agent => __FILE__
+        agent => __FILE__,
     );
 
-    $mech->get_ok( $site );
+    $mech->get( $site );
+    if ($success) {
+        ok(  $mech->success, "Should get $site successfully" );
+    } else {
+        ok( !$mech->success, "Should get $site unsuccessfully" );
+    }
     $mech->base_is( $base_is, "The base for <$site> is <$base_is>");
     $mech->content_like( $content_like, "$site has content like $content_like");
 
