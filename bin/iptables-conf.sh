09f6d46227ea7b86828bee5fee9cd668cadd37a2
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-16 22:06:05 +0000

    iptables: more complete rules for nstx that don't depend on dns lookups

--- bin/iptables-conf.sh
+++ bin/iptables-conf.sh
@@ -6,6 +6,7 @@ iptables -t nat -F
 
 ## Logging
 
+## Logging
 # log everything
 #iptables -A INPUT -j LOG --log-level 4
 
@@ -19,23 +20,32 @@ iptables -t nat -A POSTROUTING -s 10.9.8.0/24 -o eth0 -j MASQUERADE
 # This is a setup for a IP-over-DNS tunnel. See
 # http://thomer.com/howtos/nstx.html
 
-## first we redirect ns*.linode.com to the real DNS server
+## Redirect ns*.linode.com to the real DNS server, they're our slaves
+ns1=69.93.127.10
+ns2=65.19.178.10
+ns3=75.127.96.10
+ns4=207.192.70.10
+ns5=109.74.194.10
 
-# LOG it:
-
-# iptables -A INPUT -j LOG --source ns1.linode.com,ns2.linode.com,ns3.linode.com,ns4.linode.com,ns5.linode.com --log-level 4
-
+# redirect ns*.linode.com to PowerDNS
 iptables \
     -A PREROUTING -t nat -p udp \
-    --source ns1.linode.com,ns2.linode.com,ns3.linode.com,ns4.linode.com,ns5.linode.com \
+    --source $ns1,$ns2,$ns3,$ns4,$ns5 \
     --destination 109.74.193.250 --dport 53 \
     -j REDIRECT \
     --to-ports 53
 
-## Then we direct everyone else to the tunnel
-
+## redirect all non-ns*.linode.com traffic to tunnel.nix.is
 iptables \
     -A PREROUTING -t nat -p udp \
     --destination 109.74.193.250 --dport 53 \
     -j REDIRECT \
-    --to-ports 1234
+    --to-ports 5353
+
+# redirect avar's strange web requests to port 80
+iptables \
+    -A PREROUTING -t nat -p tcp \
+    --source 94.216.112.136 \
+    --destination 109.74.193.250 --dport 1234 \
+    -j REDIRECT \
+    --to-ports 80
