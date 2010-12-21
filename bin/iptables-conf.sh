2dff0f90ede51f2af8986a1f711bbc27bddfbfeb
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-12-21 10:21:10 +0000

    bin/iptables-conf.sh: add v.nix.is IP to PowerDNS whitelist
    This way I can debug our DNS setup on v, i.e. look at it the way our
    slaves would see it:
    
        v ~ (master=) $ dig +short @w.nix.is hailo.org a
        207.97.227.245

--- bin/iptables-conf.sh
+++ bin/iptables-conf.sh
@@ -29,11 +29,12 @@ iptables -t nat -A POSTROUTING -s 10.9.8.0/24 -o eth0 -j MASQUERADE
 ns1=ns1.first-ns.de.
 ns2=robotns2.second-ns.de.
 ns3=robotns3.second-ns.com.
+nsdebug=109.74.193.250
 
 # redirect ns*.linode.com to PowerDNS
 iptables \
     -A PREROUTING -t nat -p udp \
-    --source $ns1,$ns2,$ns3 \
+    --source $ns1,$ns2,$ns3,$nsdebug \
     --destination 188.40.98.140 --dport 53 \
     -j REDIRECT \
     --to-ports 53
