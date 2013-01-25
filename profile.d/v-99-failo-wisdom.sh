fba22927bf36c505a2c377d660aa361fdd32170f
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2013-01-25 18:43:37 +0000

    Don't spread wisdom to those who like silent logins

--- profile.d/v-99-failo-wisdom.sh
+++ profile.d/v-99-failo-wisdom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # spread failo's wisdom to interactive shells
-if [[ $- == *i* ]] && [[ "$(groups)" != *gcf* ]]; then
+if [[ $- == *i* ]] && [[ ! -f "$HOME/.hushlogin" ]]; then
     failo-wisdom | cowsay -f sodomized-sheep
 fi
