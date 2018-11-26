fddbdf89418f565ff76ddc65d19630ed6975c9a2
root <root@nix.is>
2018-11-26 13:11:05 +0100

    sync script: also sync /dns
    As noted on IRC I deleted enough things from 'complete' that this can
    be synced, and it'll make it easier to duplicate the old setup.

--- bin/rsync-from-unixis.sh
+++ bin/rsync-from-unixis.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
+
+rsync -av --progress --one-file-system --delete \
+	--exclude=/dns \
+	$@ \
+	u.nix.is:/ \
+	/backup/unixis/
 
 rsync -av --progress --one-file-system --delete \
 	$@ \
-	u.nix.is:/ \
+	u.nix.is:/dns \
 	/backup/unixis/
