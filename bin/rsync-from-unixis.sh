7807042e2d887c6bc80105c120e271e4af43ca91
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-11-27 10:24:10 +0100

    sync script: forgot to backup /boot

--- bin/rsync-from-unixis.sh
+++ bin/rsync-from-unixis.sh
@@ -4,11 +4,15 @@ set -euo pipefail
 
 rsync -av --progress --one-file-system --delete \
 	--exclude=/dns \
+	--exclude=/boot \
 	$@ \
 	u.nix.is:/ \
 	/backup/unixis/
 
-rsync -av --progress --one-file-system --delete \
-	$@ \
-	u.nix.is:/dns \
-	/backup/unixis/
+for partition in /boot /dns
+do
+	rsync -av --progress --one-file-system --delete \
+		$@ \
+		u.nix.is:$partition \
+		/backup/unixis/
+done
